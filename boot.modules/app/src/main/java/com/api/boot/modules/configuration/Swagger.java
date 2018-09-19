7fee29531c6bd606b8d1f4dd0db08593d24fc9d4
vonslience <vorjohnl@gmail.com>
2018-09-19 17:06:49 +0800

    " swagger modify  "

--- boot.modules/app/src/main/java/com/api/boot/modules/configuration/Swagger.java
+++ boot.modules/app/src/main/java/com/api/boot/modules/configuration/Swagger.java
@@ -1,8 +1,10 @@
 package com.api.boot.modules.configuration;
 
+import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,6 +21,9 @@ public class Swagger  {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Sets.newHashSet(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .consumes(Sets.newHashSet(MediaType.APPLICATION_FORM_URLENCODED_VALUE,MediaType.APPLICATION_JSON_VALUE))
+                .protocols(Sets.newHashSet("http","https"))
                 .apiInfo(api())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.api.boot.modules.handler"))
